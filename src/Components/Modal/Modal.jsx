72325b3207e6b96b126355dc63b74028b09a417b
tuniet <tofeju@gmail.com>
2024-02-01 16:06:06 +0100

    added modal content as prop

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -7,12 +7,13 @@ function Modal(props) {
     function handleClose() {
         props.setinmodal(false);
     }
-
+    function Content(){
+      return props.content
+    }
   return (
     <div className={`contact-modal ${props.active}`}>
         <span className='modal-close-button' onClick={handleClose}><IoMdClose /></span>
-        <h2 className='modal-title'>TITLE</h2>
-        <p className='modal-txt'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet ligula laoreet, euismod felis pellentesque, iaculis orci. Quisque hendrerit orci lacus, eleifend consequat mi egestas id. Etiam nunc arcu, dignissim vitae urna in, mattis efficitur erat. Cras vel leo a nisi interdum sodales vel quis tortor. Fusce posuere pulvinar mollis. Suspendisse at metus vitae nibh viverra venenatis ut a eros. Ut ac commodo nibh. Pellentesque nec placerat orci, id aliquam augue. Cras non luctus lacus. Integer ac velit sed eros dapibus tincidunt.</p>
+        <Content />
         
     </div>
   );
--- src/Components/Modal/Modalbutton.jsx
+++ src/Components/Modal/Modalbutton.jsx
@@ -20,11 +20,19 @@ function Modalwithbutton(props) {
     function handleclose(){
         setinmodal(false)
     }
+    function Content(){
+      return(
+        <div className='modal-content'>
+          <h2 className='modal-title'>TITLE</h2>
+          <p className='modal-txt'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet ligula laoreet, euismod felis pellentesque, iaculis orci. Quisque hendrerit orci lacus, eleifend consequat mi egestas id. Etiam nunc arcu, dignissim vitae urna in, mattis efficitur erat. Cras vel leo a nisi interdum sodales vel quis tortor. Fusce posuere pulvinar mollis. Suspendisse at metus vitae nibh viverra venenatis ut a eros. Ut ac commodo nibh. Pellentesque nec placerat orci, id aliquam augue. Cras non luctus lacus. Integer ac velit sed eros dapibus tincidunt.</p>
+        </div>
+      )
+    }
   return (
     <div>
         <button onClick={handleopen}>OPEN MODAL</button>
         <div className={`overlay-bg ${modalactive}`} onClick={handleclose}></div>
-        <Modal active = {modalactive} setinmodal={setinmodal}/>
+        <Modal active = {modalactive} setinmodal={setinmodal} content = {Content()}/>
     </div>
   );
 }
